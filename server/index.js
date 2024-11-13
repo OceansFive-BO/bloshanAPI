8b6df87e58dd5e2cc9d887edc6b9508f55d73bd1
itsWalkerRichard <walker@teamrichard.com>
2024-11-12 18:31:07 -0700

    init of express server for book api

---
+++ server/index.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import morgan from 'morgan';
+
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(3000, () => {});
