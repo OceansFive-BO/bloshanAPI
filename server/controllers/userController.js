854a734e9a50fd5bac5cda2ad113b3ebb3c45192
miandevz <simguoc@gmail.com>
2024-11-14 16:13:25 -0500

    update schema

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,7 +2,7 @@ import { User } from '../db/models.js';
 
 export const getUsers = async (req, res) => {
   try {
-    const users = await User.find().select('-password -_id');;
+    const users = await User.find().select('-password');
     res.status(200).json(users);
   } catch (error) {
     console.error('Error fetching users:', error);
