2a9af8817d44ce52ad382ab73f3a8b3a76148927
mth830 <mth830@gmail.com>
2024-11-14 20:16:37 -0500

    implemented borrowed and lending books route

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,8 +1,11 @@
 import express from 'express';
-import { getUsers } from '../controllers/userController.js';
+import { getUsers, getUserById, getBorrowedBooksByUserId, getLentBooksByUserId } from '../controllers/userController.js';
 
 const router = express.Router();
 
 router.get('/', getUsers);
+router.get('/:id', getUserById);
+router.get('/:id/borrowed', getBorrowedBooksByUserId);
+router.get('/:id/lent', getLentBooksByUserId);
 
 export default router;
