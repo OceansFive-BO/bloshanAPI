e7634af7d3544d1443fbdeab2fc9c3d0bbea18ab
miandevz <simguoc@gmail.com>
2024-11-20 12:48:10 -0500

    create getUserByEmail route

--- server/db/models.js
+++ server/db/models.js
@@ -3,7 +3,6 @@ import mongoose from 'mongoose';
 const UserSchema = new mongoose.Schema({
   firstname: { type: String, required: true },
   lastname: { type: String, required: true },
-  password: { type: String, required: true },
   username: { type: String, required: true, unique: true,index: true },
   phone: { type: String, required: true },
   email: { type: String, required: true },
